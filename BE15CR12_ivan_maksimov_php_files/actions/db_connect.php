d3e08b0c5d842cb566bd48bcc377ee69dbfb8f34
ivmaksimov <100725158+ivmaksimov@users.noreply.github.com>
2022-04-06 14:22:34 +0200

    the new db_connect added

--- BE15CR12_ivan_maksimov_php_files/actions/db_connect.php
+++ BE15CR12_ivan_maksimov_php_files/actions/db_connect.php
@@ -10,6 +10,4 @@ $connect = mysqli_connect($localhost, $username, $password, $dbname);
 // check connection
 if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully";
 }
